4846a81bce0548f2a73fb7e6d58838c571c2271a
Foster Caragay <facaragay@Gmail.com>
2021-10-27 10:39:10 -0400

    Updated Names

--- Rotator Serial Interface/Properties/Resources.Designer.cs
+++ Rotator Serial Interface/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Rotator_Serial_Interface.Properties {
+namespace SatNogs_Rotator_Control_Tool.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Rotator_Serial_Interface.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Rotator_Serial_Interface.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SatNogs_Rotator_Control_Tool.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
